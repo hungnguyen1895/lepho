01d5d2350c92cb8b753bd003dc576a2d205914f2
Hung Nguyen <hn9581@gmail.com>
2020-10-20 23:45:38 -0400

    Init the app

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Le Pho
         </a>
       </header>
     </div>
